9ce096dfbae5e3d15963c50ac6f5f208f5d56c33
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-10-17 16:22:59 +0200

    reading scierc data

--- reading_scierc_data.py
+++ reading_scierc_data.py
@@ -5,20 +5,21 @@ from util import data_io
 
 TAG_TYPE = 'ner'
 LABELS = ['Generic','Task','Method','Material','Metric','OtherScientificTerm']
-def build_flair_sentences(d: Dict) -> List[Sentence]:
 
-    def prefix_to_BIOES(label,start,end,current_index):
-        if end - start > 0:
-            if current_index == start:
-                prefix = 'B'
-            elif current_index == end:
-                prefix = 'E'
-            else:
-                prefix = 'I'
+def prefix_to_BIOES(label, start, end, current_index):
+    if end - start > 0:
+        if current_index == start:
+            prefix = 'B'
+        elif current_index == end:
+            prefix = 'E'
         else:
-            prefix = 'S'
+            prefix = 'I'
+    else:
+        prefix = 'S'
 
-        return prefix+'-'+label
+    return prefix + '-' + label
+
+def build_flair_sentences(d: Dict) -> List[Sentence]:
 
     def tag_it(token:Token,index,ner_spans):
         labels = [(start,end,label) for start,end,label in ner_spans if index>=start and index<=end]
@@ -73,7 +74,39 @@ def build_tag_dict(sentences:List[Sentence],tag_type):
     # tag_dictionary.add_item("<STOP>")
     return corpus.make_tag_dictionary(tag_type)
 
+def another_span_is_wider(s,k, spans):
+    return any([(s[0] >= o[0]) and (s[1] <= o[1]) and k != i for i,o in enumerate(spans)])
+
+def build_sequences(d:dict):
+    def build_tag(index,ner_spans)->str:
+        spans_overlapping_with_index = [(start,end,label) for start,end,label in ner_spans if index>=start and index<=end]
+        assert len(spans_overlapping_with_index)<=1
+        if len(spans_overlapping_with_index)==1:
+            start, end, label = spans_overlapping_with_index[0]
+            tag = prefix_to_BIOES(label, start, end, index)
+        else:
+            tag = 'O'
+        return tag
+
+    tagged_seqs = []
+    offset = 0
+    for tokens, token_spans in zip(d['sentences'], d['ner']):
+        token_spans = [s for k,s in enumerate(token_spans) if not another_span_is_wider(s,k, token_spans)]
+
+        assert all([l in LABELS for _,_,l in token_spans])
+        tagged_seqs.append([(token,build_tag(token_index + offset, token_spans)) for token_index, token in enumerate(tokens)])
+        offset += len(tokens)
+    return tagged_seqs
+
+def read_scierc_seqs(jsonl_file):
+    seqs = [sent
+     for d in data_io.read_jsonl(jsonl_file)
+     for sent in build_sequences(d)]
+    return seqs
+
 if __name__ == '__main__':
-    file = '../data/processed_data/json/dev.json'
-    print(read_scierc_file_to_FlairSentences(file))
+    from pathlib import Path
+    home = str(Path.home())
+    file = home+'/data/scierc_data/processed_data/json/dev.json'
+    read_scierc_seqs(file)
 
