705c450fcffb9a5ab7914381a4fada25b334a193
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-07 19:39:15 +0200

    learning_curve.py results folder

--- learning_curve.py
+++ learning_curve.py
@@ -1,7 +1,11 @@
 import os
 from collections import OrderedDict
 from functools import partial
-from mlutil.crossvalidation import calc_scores, calc_mean_and_std, ScoreTask #TODO(tilo) must be imported before numpy
+from mlutil.crossvalidation import (
+    calc_scores,
+    calc_mean_and_std,
+    ScoreTask,
+)  # TODO(tilo) must be imported before numpy
 
 from eval_jobs import crosseval_on_concat_dataset_trainsize_range
 import numpy
@@ -10,11 +14,13 @@ from time import time
 from typing import Dict, List, Tuple, Any, Iterable
 from sklearn.model_selection import ShuffleSplit
 
-from benchmark_flair_tagger import score_flair_tagger, \
-    kwargs_builder_maintaining_train_dev_test, kwargs_builder
+from benchmark_flair_tagger import (
+    score_flair_tagger,
+    kwargs_builder_maintaining_train_dev_test,
+    kwargs_builder,
+)
 from reading_seqtag_data import read_scierc_data, read_JNLPBA_data, TaggedSeqsDataSet
 from util import data_io
-
 
 
 def groupandsort_by_first(tups: Iterable[Tuple[Any, Any]]):
@@ -44,28 +50,31 @@ def calc_write_learning_curve(
     n_jobs=0,
     results_path=home + "/data/seqtag_results",
 ):
-    os.makedirs(results_path,exist_ok=True)
+    results_path = results_path + "/" + name
+    os.makedirs(results_path, exist_ok=True)
     start = time()
     task = ScoreTask(scorer_fun, kwargs_builder, params)
 
     scores = calc_scores(task, [split for train_size, split in splits], n_jobs=n_jobs)
-    print(
-        "calculating learning-curve for %s took %0.2f seconds" % (name, time() - start)
-    )
+    duration = time() - start
+    meta_data = {
+        "duration": duration,
+        "num-workers": n_jobs,
+    }
+    data_io.write_json(results_path + "/meta_datas.json", meta_data)
+    print("calculating learning-curve for %s took %0.2f seconds" % (name, duration))
     results = groupandsort_by_first(
         zip([train_size for train_size, _ in splits], scores)
     )
-    data_io.write_json(results_path + "/learning_curve_%s.json" % name, results)
+    data_io.write_json(results_path + "/learning_curve.json", results)
 
     trainsize_to_mean_std_scores = {
         train_size: tuple_2_dict(calc_mean_and_std(m))
         for train_size, m in results.items()
     }
     data_io.write_json(
-        results_path + "/learning_curve_meanstd_%s.json" % name,
-        trainsize_to_mean_std_scores,
+        results_path + "/learning_curve_meanstd.json", trainsize_to_mean_std_scores,
     )
-
 
 
 def spacyCrfSuite_kwargs_supplier(params, data_supplier):
@@ -92,7 +101,6 @@ def spacyCrfSuite_kwargs_supplier_single_set(params, data_supplier):
     }
 
 
-
 if __name__ == "__main__":
     # data_supplier= partial(read_scierc_data,path=home + "/data/scierc_data/sciERC_processed/processed_data/json")
     # data_supplier = partial(
@@ -102,23 +110,24 @@ if __name__ == "__main__":
     # )
     data_path = os.environ["HOME"] + "/scibert/data/ner/JNLPBA"
     # data_path = os.environ["HOME"] + "/code/misc/scibert/data/ner/JNLPBA"
-    data_supplier = partial(read_JNLPBA_data,path=data_path)
+    data_supplier = partial(read_JNLPBA_data, path=data_path)
 
-    dataset:TaggedSeqsDataSet = data_supplier()
+    dataset: TaggedSeqsDataSet = data_supplier()
     sentences = dataset.train + dataset.dev + dataset.test
     dataset_size = len(sentences)
 
-
     num_folds = 3
     # splits = build_splits(dataset,num_folds)
-    splits = crosseval_on_concat_dataset_trainsize_range(dataset_size, num_folds=2,test_size=0.2,starts=0.05,ends=0.1,steps=0.05)
+    splits = crosseval_on_concat_dataset_trainsize_range(
+        dataset_size, num_folds=2, test_size=0.2, starts=0.05, ends=0.1, steps=0.05
+    )
     print("got %d evaluations to calculate" % len(splits))
 
     calc_write_learning_curve(
-        "flair",
+        "flair-minimal-test",
         kwargs_builder,
         score_flair_tagger,
-        {"params": {"max_epochs": 2},'data_supplier':data_supplier},
+        {"params": {"max_epochs": 2}, "data_supplier": data_supplier},
         splits,
         2,
     )
