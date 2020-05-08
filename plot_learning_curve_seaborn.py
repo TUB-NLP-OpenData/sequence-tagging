aa634fdf6c43827d169f8d836b91911d0ae47542
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-08 11:50:06 +0200

    build_method_name

--- plot_learning_curve_seaborn.py
+++ plot_learning_curve_seaborn.py
@@ -4,21 +4,21 @@ import pandas as pd
 from util import data_io
 
 
-def plot_learncurve(data_path, files, split_name, save_dir="images"):
-    def build_method_name(file):
-        return file.split("_")[-1].replace(".json", "")
+def plot_learncurve(paths, split_name, save_dir="images"):
+    def build_method_name(path):
+        return path.split('/')[-1]
 
-    methods = [build_method_name(f) for f in files]
+    methods = [build_method_name(f) for f in paths]
 
     sns.set(style="ticks", palette="pastel")
     data = [
         {
             "train_size": round(float(train_size), 2),
             "f1-micro-spanlevel": score[split_name]["f1-micro-spanlevel"],
-            "method": build_method_name(file),
+            "method": build_method_name(path),
         }
-        for file in files
-        for train_size, scores in data_io.read_json(data_path + "/" + file).items()
+        for path in paths
+        for train_size, scores in data_io.read_json(path + "/learning_curve.json").items()
         for score in scores
     ]
     num_cross_val = len(data) / len(set([d["train_size"] for d in data])) / len(methods)
@@ -43,10 +43,7 @@ if __name__ == "__main__":
     import seaborn as sns
 
     home = os.environ['HOME']
-    data_path = home + "/data/scierc_data"
+    data_path = home + "/hpc/data/seqtag_results/spacy-crf-15-workers"
 
-    files = ['learning_curve_spacyCrfSuite.json']
-    # files = ['learning_curve_flair.json', 'learning_curve_spacyCrfSuite.json']
-    # files = ["learning_curve_flair.json"]
-    plot_learncurve(data_path, files, "train", save_dir=".")
-    plot_learncurve(data_path, files, "test", save_dir=".")
+    plot_learncurve([data_path], "train", save_dir=".")
+    plot_learncurve([data_path], "test", save_dir=".")
