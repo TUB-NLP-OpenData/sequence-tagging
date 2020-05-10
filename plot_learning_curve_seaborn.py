c2c82bba0c6a7d379e4e6f5ea0fe18ba6e501ca6
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-10 11:25:51 +0200

    figsize

--- plot_learning_curve_seaborn.py
+++ plot_learning_curve_seaborn.py
@@ -1,15 +1,17 @@
 import os
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 from util import data_io
 
 
 def plot_learncurve(paths, split_name, save_dir="images"):
     def build_method_name(path):
-        return path.split('/')[-1]
+        return path.split("/")[-1]
 
     methods = [build_method_name(f) for f in paths]
-
+    fig, ax = plt.subplots(figsize=(5, 10))
     sns.set(style="ticks", palette="pastel")
     data = [
         {
@@ -18,13 +20,20 @@ def plot_learncurve(paths, split_name, save_dir="images"):
             "method": build_method_name(path),
         }
         for path in paths
-        for train_size, scores in data_io.read_json(path + "/learning_curve.json").items()
+        for train_size, scores in data_io.read_json(
+            path + "/learning_curve.json"
+        ).items()
         for score in scores
     ]
     num_cross_val = len(data) / len(set([d["train_size"] for d in data])) / len(methods)
     df = pd.DataFrame(data=data)
     ax = sns.boxplot(
-        x="train_size", y="f1-micro-spanlevel", hue="method", palette=["m", "g"], data=df
+        ax=ax,
+        x="train_size",
+        y="f1-micro-spanlevel",
+        hue="method",
+        palette=["m", "g", "r"],
+        data=df,
     )
     # sns.despine(offset=10, trim=True)
     ax.set_title(
@@ -34,16 +43,19 @@ def plot_learncurve(paths, split_name, save_dir="images"):
     ax.figure.savefig(
         save_dir + "/learning_curve_%s_%s.png" % (split_name, "-".join(methods))
     )
-    from matplotlib import pyplot as plt
 
     plt.close()
 
 
 if __name__ == "__main__":
-    import seaborn as sns
 
-    home = os.environ['HOME']
-    data_path = home + "/hpc/data/seqtag_results/spacy-crf-15-workers"
+    home = os.environ["HOME"]
+    data_path = home + "/hpc/data/seqtag_results"
+    result_paths = [
+        data_path + "/" + p
+        for p in os.listdir(data_path)
+        if os.path.isdir(data_path + "/" + p)
+    ]
 
-    plot_learncurve([data_path], "train", save_dir=".")
-    plot_learncurve([data_path], "test", save_dir=".")
+    plot_learncurve(result_paths, "train", save_dir=".")
+    plot_learncurve(result_paths, "test", save_dir=".")
